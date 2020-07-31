7b1fff3c82f18ad37d70b78f676bb807019ac633
d.vologzhanin <dsvologzhanin@gmail.com>
2020-07-31 18:40:08 +0300

    решение задачи про сравнение букв и повышения 1 буквы в строке

--- src/main/java/javastringintroduction/Main.java
+++ src/main/java/javastringintroduction/Main.java
@@ -1,4 +1,19 @@
 package javastringintroduction;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a.length() + b.length());
+
+        if(a.toLowerCase().charAt(0) > b.toLowerCase().charAt(0)){
+            System.out.println("Yes"); }
+        else {System.out.println("No");}
+
+        System.out.println(a.substring(0,1).toUpperCase() + a.substring(1)+ " " + b.substring(0,1).toUpperCase() + b.substring(1));
+    }
+
 }
